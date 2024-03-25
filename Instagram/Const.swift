14df7ab743783cf2d4c313908c7164c4b0c4bef5
atsushi.hamada <tomato8181@gmail.com>
2024-03-25 20:50:05 +0900

    20240325 20:50

---
+++ Instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by cpcadmin on 2024/03/25.
+//
+
+import Foundation
+
+struct Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+}
