6206c22865aee28407071c2bea9e6dceea55abc3
atsushi <tomato8181@gmail.com>
2024-03-29 17:29:17 +0900

    3/29 17:30

---
+++ Instagram/PostTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PostTableViewCell.swift
+//  Instagram
+//
+//  Created by 濱田純史 on 2024/03/29.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
